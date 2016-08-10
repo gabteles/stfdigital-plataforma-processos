bda62aceae70d731ed103ccf0d47854b274ffba2
Rodrigo Barreiros <barreiros.rodrigo@gmail.com>
2016-08-10 17:17:07 -0300

    Ajustes nas configurações de segurança dos testes unitários e dos testes de integração

--- src/test/java/br/jus/stf/core/processos/PesquisaProcessosIntegrationTests.java
+++ src/test/java/br/jus/stf/core/processos/PesquisaProcessosIntegrationTests.java
@@ -1,17 +1,22 @@
 package br.jus.stf.core.processos;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 
 import br.jus.stf.core.ApplicationContextInitializer;
+import br.jus.stf.core.framework.component.ComponentConfig;
+import br.jus.stf.core.framework.component.suggestion.SuggestionConfig;
+import br.jus.stf.core.framework.security.SecurityChecker;
 import br.jus.stf.core.framework.testing.IntegrationTestsSupport;
 import br.jus.stf.core.framework.testing.oauth2.WithMockOauth2User;
 import br.jus.stf.core.processos.domain.Processo;
@@ -29,6 +34,9 @@ public class PesquisaProcessosIntegrationTests extends IntegrationTestsSupport {
 	@Autowired
 	private ElasticsearchTemplate elasticsearchTemplate;
 	
+    @MockBean
+    private SecurityChecker securityChecker;
+
     @Test
     public void deveRejeitarUmaPesquisaInvalida() throws Exception {
         mockMvc.perform(post("/api/processos/pesquisa-avancada").contentType(APPLICATION_JSON).content("{}")).andExpect(status().isBadRequest());
@@ -36,6 +44,7 @@ public class PesquisaProcessosIntegrationTests extends IntegrationTestsSupport {
     
     @Test
     public void deveSugerirUmProcesso() throws Exception {
+		given(securityChecker.hasPermission(newSuggestion())).willReturn(true);
     	
     	Processo processo = new Processo(1L, "1");
     	processo.setId("teste");
@@ -57,5 +66,11 @@ public class PesquisaProcessosIntegrationTests extends IntegrationTestsSupport {
         
         elasticsearchTemplate.delete(Processo.class, "teste");
     }
+
+	private ComponentConfig newSuggestion() {
+		SuggestionConfig suggestionConfig = new SuggestionConfig();
+		suggestionConfig.setId("pesquisar-processos");
+		return suggestionConfig;
+	}
     
 }
